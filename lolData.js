791b4dba21c301fe75b2bfc576390ddfeedda223
unknown <Adrien49.b@gmail.com>
2019-11-27 18:46:28 +0100

    ajout bootstrap + dossier img

--- lolData.js
+++ lolData.js
@@ -1,7 +1,7 @@
 var urlAPI = "https://euw1.api.riotgames.com/lol/";
-var APIkey = "RGAPI-0cd586f6-594b-4795-8f17-b7628a1fa394"
+var APIkey = "RGAPI-23577392-4902-4110-94fb-5fbcedc3ac4c"
 
-function getName(){
+function getInfos(){
 
 var summonerName = $('#name').val();
 console.log(summonerName);
@@ -13,8 +13,7 @@ $.ajax({
     
     success: function(data){
         $('#test').append("<p>Summoner's name: " + data.name + "</p> <p>Level " + data.summonerLevel + "</p>");        
-        
-    console.log(data);
+        console.log(data.id);
 
     $.ajax({ 
         type: "GET",
@@ -23,12 +22,12 @@ $.ajax({
         
         success: function(infoRanked){
             infoRanked.forEach(test => {
-                $('#infoRanked').append("<br>TYPE:  " + test.queueType + " " + test.tier + " " + test.rank + " " + test.leaguePoints + "LP");
+                $('#infoRanked').append("<li class= list-group-item> TYPE:  " + test.queueType + " " + test.tier + " " + test.rank + " " + test.leaguePoints + "LP </li>");
                 if(test.miniSeries != null){
-                    $("#infoRanked").append("<p>Promotion: " + test.miniSeries.wins + "W/" + test.miniSeries.losses+ "L </p>" ); 
+                    $("#infoRanked").append("<li class= list-group-item> Promotion: " + test.miniSeries.wins + "W/" + test.miniSeries.losses+ "L </li>" ); 
                 }
-                var winrate = (test.wins/ (test.wins + test.losses))*100;
-                $('#infoRanked').append("<br>wins: " + test.wins + "<br> losses: " + test.losses + "<br> winrate: " + winrate + "%");
+                var winrate = parseInt((test.wins/ (test.wins + test.losses))*100);
+                $('#infoRanked').append("<li class= list-group-item> wins: " + test.wins + "<br> losses: " + test.losses + "<br> winrate: " + winrate + "% </li>");
                 console.log(winrate);
             });
             
